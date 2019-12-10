b860e041c8f36c1bfcf0f01e917e905d9bcb2e27
Lucas Silva <jacks2kar@gmail.com>
2019-12-10 00:49:08 -0300

    autenticação de usuário

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: "0f208ad0c7718f49dcf4a0bba528866a",
+  expiresIn: "7d",
+}
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware); // Como ele está vindo após as rotas acima, ele só
+//vale para as rotas que vem apos ele
+
+routes.put('/users', UserController.update);
 
 export default routes;
