299cd9b7e562d761284240066edd1fa6c91d42a5
Lucas Silva <jacks2kar@gmail.com>
2020-03-02 15:44:57 -0300

    Configurando nodemailer

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,10 +1,12 @@
+import * as Yup from "yup";
+import { startOfHour, parseISO, isBefore, format, subHours } from "date-fns";
+import pt from "date-fns/locale/pt";
 import User from "../models/User";
 import File from "../models/File";
 import Appointment from "../models/Appointment";
 import Notification from "../schemas/Notification";
-import * as Yup from "yup";
-import { startOfHour, parseISO, isBefore, format, subHours } from "date-fns";
-import pt from "date-fns/locale/pt";
+
+import Mail from "../../lib/Mail";
 
 class AppointmentController {
   async index(req, res) {
@@ -111,7 +113,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: "provider",
+          attributes: ["name", "email"]
+        }
+      ]
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.sttus(401).json({
@@ -131,6 +141,12 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: "Agendamento cancelado",
+      text: "Você tem um novo cancelamento"
+    });
 
     return res.json(appointment);
   }
---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: "2525",
+  secure: false,
+  auth: {
+    user: "487cb66cec35ae",
+    pass: "9f3dfa58e7746b"
+  },
+  default: {
+    from: "Equipe GoBarber <noreply@gobarber.com>"
+  }
+};
